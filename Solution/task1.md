320e1cccd921807bb026336db364f37bf012bc41
igor_pyatuha <71142388+igor-hub-maker@users.noreply.github.com>
2024-01-19 12:06:23 +0000

    task done

---
+++ Solution/task1.md
@@ -0,0 +1,6 @@
+## task1.1
+![Alt text](task1.1.png)
+## task1.2
+![Alt text](task1.2.png)
+## На 5 балів
+![Alt text](task1.3.png)
---
+++ src/Quotation.java
@@ -0,0 +1,8 @@
+public class Quotation {
+    String quote = "Хочу... повернутись до землі.";
+    String author = "Мешл, з аніме \"Магія та мускули\"";
+    public void display() {
+      System.out.println(quote);
+      System.out.println("Автор: " +author);
+    }
+  }
